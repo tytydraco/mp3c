68efb63bf1c3bb0cc610a4cbb23723b746b69b52
Tyler Nijmeh <tylernij@gmail.com>
2026-05-25 18:14:05 -0500

    Explicit cclock
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- converters/video/convert_video_generic_avi.sh
+++ converters/video/convert_video_generic_avi.sh
@@ -27,36 +27,36 @@ function convert_video_generic_avi() {
 
     local size="'if(gt(ih, iw), $width, $height)':'if(gt(ih, iw), $height, $width)'"
     local ffmpeg_args=(
-        -n                                                                                                                                  # Do not replace existing files.
-        -f avi                                                                                                                              # AVI container.
-        -c:v libx264                                                                                                                        # H.264 codec.
-        -profile:v baseline                                                                                                                 # H.264 baseline profile.
-        -filter:v "scale=$size:force_original_aspect_ratio=decrease,pad=$size:(ow-iw)/2:(oh-ih)/2:black,transpose=2:passthrough=portrait"   # Contain within size, preserve aspect ratio, pad, pre-rotate (portrait bypass).
-        -pix_fmt:v yuv420p                                                                                                                  # yuv420p pixel format.
-        -bufsize:v "$bufsize"                                                                                                               # Hardware buffer size.
-        -maxrate:v "$maxrate"                                                                                                               # Limit bitrate.
-        -g:v "$maxfps"                                                                                                                      # GOP length every 1 second.
-        -b:v "$bitrate"                                                                                                                     # Target average bitrate.
-        -fpsmax:v "$maxfps"                                                                                                                 # Limit FPS.
-        -c:a pcm_s16le                                                                                                                      # 16-bit PCM audio codec.
-        -ac:a 1                                                                                                                             # Mono audio.
+        -n                                                                                                                                      # Do not replace existing files.
+        -f avi                                                                                                                                  # AVI container.
+        -c:v libx264                                                                                                                            # H.264 codec.
+        -profile:v baseline                                                                                                                     # H.264 baseline profile.
+        -filter:v "scale=$size:force_original_aspect_ratio=decrease,pad=$size:(ow-iw)/2:(oh-ih)/2:black,transpose=cclock:passthrough=portrait"  # Contain within size, preserve aspect ratio, pad, pre-rotate counter-clockwise (portrait bypass).
+        -pix_fmt:v yuv420p                                                                                                                      # yuv420p pixel format.
+        -bufsize:v "$bufsize"                                                                                                                   # Hardware buffer size.
+        -maxrate:v "$maxrate"                                                                                                                   # Limit bitrate.
+        -g:v "$maxfps"                                                                                                                          # GOP length every 1 second.
+        -b:v "$bitrate"                                                                                                                         # Target average bitrate.
+        -fpsmax:v "$maxfps"                                                                                                                     # Limit FPS.
+        -c:a pcm_s16le                                                                                                                          # 16-bit PCM audio codec.
+        -ac:a 1                                                                                                                                 # Mono audio.
     )
 
     local ffmpeg_map_args=()
     if has_audio "$input_file"; then
         ffmpeg_map_args=(
-            -map 0:v:0                                                                                                                      # Choose first video stream.
-            -map 0:a:0                                                                                                                      # Choose first audio stream.
-            -ar:a 22050                                                                                                                     # 22.05 kHz audio rate.
+            -map 0:v:0                                                                                                                          # Choose first video stream.
+            -map 0:a:0                                                                                                                          # Choose first audio stream.
+            -ar:a 22050                                                                                                                         # 22.05 kHz audio rate.
         )
     else
         ffmpeg_map_args=(
-            -f lavfi                                                                                                                        # Virtual audio device.
-            -i "anullsrc=channel_layout=mono:sample_rate=8000"                                                                              # 8 kHz silent audio.
-            -map 0:v:0                                                                                                                      # Choose first video stream.
-            -map 1:a                                                                                                                        # Include silent audio.
-            -ar:a 8000                                                                                                                      # 8 kHz audio rate.
-            -shortest                                                                                                                       # Stop encoding when video stream ends.
+            -f lavfi                                                                                                                            # Virtual audio device.
+            -i "anullsrc=channel_layout=mono:sample_rate=8000"                                                                                  # 8 kHz silent audio.
+            -map 0:v:0                                                                                                                          # Choose first video stream.
+            -map 1:a                                                                                                                            # Include silent audio.
+            -ar:a 8000                                                                                                                          # 8 kHz audio rate.
+            -shortest                                                                                                                           # Stop encoding when video stream ends.
         )
     fi
 
