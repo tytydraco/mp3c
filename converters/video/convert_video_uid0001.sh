3c1e967b343a10402c747ae79c10b227754cca21
Tyler Nijmeh <tylernij@gmail.com>
2026-05-29 19:19:09 -0500

    Never crash again, the right way
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- converters/video/convert_video_uid0001.sh
+++ converters/video/convert_video_uid0001.sh
@@ -17,6 +17,31 @@ function convert_video_uid0001() {
             "$1" | grep -q .
     }
 
+    function fps_original() {
+        local fps_original
+
+        fps_original="$(ffprobe \
+            -v error \
+            -select_streams v:0 \
+            -show_entries "stream=avg_frame_rate" \
+            -of csv=p=0 \
+            "$1" | awk -F '/' '{ if ($2) print $1 / $2; else print $1 }')"
+        
+        echo "${fps_original:-30}"
+    }
+
+    function bufsize() {
+        local fps="$1"
+        local maxrate=10000 # 10 Mbps safe device data rate.
+
+        awk -v fps="$fps" -v maxrate="$maxrate" 'BEGIN { printf "%d", maxrate / fps }'
+    }
+
+    local fps
+    local bufsize
+    fps="$(fps_original "$input_file")"
+    bufsize="$(bufsize "$fps")"
+
     local size="'if(gt(ih, iw), 240, 288)':'if(gt(ih, iw), 288, 240)'"
     local ffmpeg_args=(
         -n                                                                                                                                      # Do not replace existing files.
@@ -25,14 +50,12 @@ function convert_video_uid0001() {
         -profile:v baseline                                                                                                                     # H.264 baseline profile.
         -filter:v "scale=$size:force_original_aspect_ratio=decrease,pad=$size:(ow-iw)/2:(oh-ih)/2:black,transpose=cclock:passthrough=portrait"  # Contain within size, preserve aspect ratio, pad, pre-rotate counter-clockwise (portrait bypass).
         -pix_fmt:v yuvj420p                                                                                                                     # Full range pixel format.
-        -bufsize:v 10M                                                                                                                          # Hardware buffer size.
+        -bufsize:v "${bufsize}K"                                                                                                                # Calculated buffer size to clamp I-frame sizes.
         -maxrate:v 10M                                                                                                                          # Limit bitrate.
-        -b:v 1M                                                                                                                                 # Target average bitrate.
-        -fpsmax:v 30                                                                                                                            # Limit FPS.
-        -g:v 30                                                                                                                                 # GOP length every 1 second.
-        -keyint_min:v 30                                                                                                                        # Allow one IDR per GOP.
-        -qmin 20
-        -sc_threshold:v 0                                                                                                                       # Disable scenecut IDRs.
+        -b:v 2M                                                                                                                                 # Target average bitrate.
+        -r:v "$fps"                                                                                                                             # Match original source FPS.
+        -g:v 1                                                                                                                                  # Only I-frames.
+        -sc_threshold:v 0                                                                                                                       # Disable scene-cut.
         -c:a pcm_s16le                                                                                                                          # 16-bit PCM audio codec.
     )
 
