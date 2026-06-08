8c631e4d6c69981e1e140c05479c2d32e05f9ab0
Tyler Nijmeh <tylernij@gmail.com>
2026-06-07 22:17:23 -0500

    Heavily optimize UID0001,2,4 quality + fixes + cleanup
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- converters/video/convert_video_uid0004.sh
+++ converters/video/convert_video_uid0004.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-FFMPEG_MOD="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")/ffmpeg-mod.exe"
-
 function convert_video_uid0004() {
     [[ -z "${1:-}" ]] && return 1
 
@@ -17,50 +15,17 @@ function convert_video_uid0004() {
             "$1" | grep -q .
     }
 
-    function fps_original() {
-        local fps_original
-
-        fps_original="$(ffprobe \
-            -v error \
-            -select_streams v:0 \
-            -show_entries "stream=avg_frame_rate" \
-            -of csv=p=0 \
-            "$1" | awk -F '/' '{ if ($2) print $1 / $2; else print $1 }')"
-        
-        echo "${fps_original:-30}"
-    }
-
-    # Scale bitrate using the frame rate, using 2 Mbps @ 30fps as a reference point.
-    function scaled_bitrate() {
-        local fps="$1"
-        
-        # reference_bitrate kbps @ reference_fps fps. Device can handle up to 10 Mbps.
-        local max_bitrate=10000
-        local reference_bitrate=1500
-        local reference_fps=30 
-        awk \
-            -v fps="$fps" \
-            -v m_bitrate="$max_bitrate" \
-            -v r_bitrate="$reference_bitrate" \
-            -v r_fps="$reference_fps" \
-            'BEGIN { v = r_bitrate * fps / r_fps; if (v > m_bitrate) v = m_bitrate; if (v < 1) v = 1; printf "%d", v }'
-    }
-
-    local fps
-    local bitrate
-    fps="$(fps_original "$input_file")"
-    bitrate="$(scaled_bitrate "$fps")"
-
     local size="'if(gt(ih, iw), 240, 320)':'if(gt(ih, iw), 320, 240)'"
-    local ffmpeg_args=(
+    local ffmpeg_args=( 
         -n                                                                                                                                      # Do not replace existing files.
         -f avi                                                                                                                                  # AVI container.
         -c:v libx264                                                                                                                            # H.264 codec.
-        -x264-params "aq-mode=3:aq-strength=0.8"                                                                                                # Auto-variance AQ for dark scenes.
+        -x264-params "ipratio=1:aq-mode=0:rc-lookahead=0"                                                                                                # Auto-variance AQ for dark scenes.
         -profile:v baseline                                                                                                                     # H.264 baseline profile.
         -filter:v "scale=$size:force_original_aspect_ratio=decrease,pad=$size:(ow-iw)/2:(oh-ih)/2:black,transpose=cclock:passthrough=portrait"  # Contain within size, preserve aspect ratio, pad, pre-rotate counter-clockwise (portrait bypass).
+        -bsf:v "filter_units=remove_types=6"                                                                                                    # Remove SEI.
         -pix_fmt:v yuvj420p                                                                                                                     # Full range pixel format.
-        -b:v "${bitrate}K"                                                                                                                      # Target average bitrate.
+        -crf:v 16                                                                                                                               # Target consistent quality.
         -fpsmax:v 30                                                                                                                            # Match original source FPS.
         -qmin:v 20                                                                                                                              # Limit I-frame complexity.
         -g:v 1                                                                                                                                  # Only I-frames.
@@ -88,11 +53,10 @@ function convert_video_uid0004() {
     fi
 
     "$FFMPEG_YP3" \
-        -i "$input_file" \
+    	-i "$input_file" \
         "${ffmpeg_map_args[@]}" \
         "${ffmpeg_args[@]}" \
         "$output_file"
 }
 
-export FFMPEG_MOD
 export -f convert_video_uid0004
