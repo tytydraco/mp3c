2f673af84efd3a30e88da9b5cf50ffe8694f2d0e
Tyler Nijmeh <tylernij@gmail.com>
2026-06-14 17:12:47 -0500

    Fix 0008 rotation images
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- converters/image/convert_image_uid0008.sh
+++ converters/image/convert_image_uid0008.sh
@@ -9,8 +9,8 @@ function convert_image_uid0008() {
     local size="640x480"
     local convert_args=(
         -interlace none     # Remove interlace.
-        -rotate "-90<"      # Rotate if needed.
-        -resize "$size>"  # Contain within size.
+        -rotate "-90>"      # Rotate if needed.
+        -resize "$size>"    # Contain within size.
     )
 
     convert \
