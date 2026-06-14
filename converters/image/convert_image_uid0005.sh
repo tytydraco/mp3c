31e12aa377e30cdc0943bd0fbe94b14831c15156
Tyler Nijmeh <tylernij@gmail.com>
2026-06-14 17:25:35 -0500

    UID0005 crop and fix unnecessary rotation
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- converters/image/convert_image_uid0005.sh
+++ converters/image/convert_image_uid0005.sh
@@ -9,10 +9,8 @@ function convert_image_uid0005() {
     local size="128x128"
     local convert_args=(
         -interlace none     # Remove interlace.
-        -rotate "-90>"      # Rotate if needed.
         -gravity center     # Center within canvas.
-        -background black   # Pad with black.
-        -resize "$size"     # Contain within size.
+        -resize "${size}^"  # Contain within size.
         -extent "$size"     # Full canvas size.
     )
 
