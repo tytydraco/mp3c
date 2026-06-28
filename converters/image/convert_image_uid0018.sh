ef4dde7503bb0184660ade7c9c069d2f97ed8abf
Tyler Nijmeh <tylernij@gmail.com>
2026-06-28 15:52:20 -0500

    Add UID0018, new variant
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

---
+++ converters/image/convert_image_uid0018.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+function convert_image_uid0018() {
+    [[ -z "${1:-}" ]] && return 1
+
+    local input_file="$1"
+    local output_file="${input_file%.*}.uid0018.jpg"
+
+    local size="240x320"
+    local convert_args=(
+        -interlace none
+        -auto-orient
+        -rotate "90>"
+        -resize "$size^"
+        -gravity center
+        -extent "$size"
+    )
+
+    convert \
+        "$input_file" \
+        "${convert_args[@]}" \
+        "$output_file"
+}
+
+export -f convert_image_uid0018
