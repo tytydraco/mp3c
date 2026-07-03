a7cc89323c604ecbeb21c79da73e62558a0e4257
Tyler Nijmeh <tylernij@gmail.com>
2026-07-03 13:58:20 -0500

    UID0018: Scale quality correctly since higher resolution
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- converters/video/convert_video_uid0018.sh
+++ converters/video/convert_video_uid0018.sh
@@ -39,7 +39,7 @@ function convert_video_uid0018() {
             crop=$size
         "
         -pix_fmt:v yuv420p
-        -q:v 4
+        -q:v 6
         -r:v "$fps"
         -c:a aac
         -ar:a 16000
