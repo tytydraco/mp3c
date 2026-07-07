0ec47eea540fbc3f2689aec2c23674f332a3c559
Tyler Nijmeh <tylernij@gmail.com>
2026-07-06 20:20:19 -0500

    Pull from new static tree
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- main.sh
+++ main.sh
@@ -7,12 +7,30 @@ WORKING_DIR="$SCRIPT_DIR/working"
 CONVERTERS_DIR="$SCRIPT_DIR/converters"
 CONFIG_FILE="$SCRIPT_DIR/config.sh"
 
-FFMPEG_YP3="$SCRIPT_DIR/tools/ffmpeg/ffmpeg-yp3-patch/static/ffmpeg" # Or: WINEDEBUG=-all wine "$SCRIPT_DIR/tools/ffmpeg/vendor/ffmpeg-mod-shenju.exe"
+FFMPEG_YP3_DIR="$SCRIPT_DIR/tools/ffmpeg/yp3_patch"
+FFMPEG_YP3="$FFMPEG_YP3_DIR/ffmpeg-amd64" # Or: WINEDEBUG=-all wine "$SCRIPT_DIR/tools/ffmpeg/vendor/ffmpeg-mod-shenju.exe"
+[[ "$(uname -m)" == "aarch64" || "$(uname -m)" == "arm64" ]] && FFMPEG_YP3="$FFMPEG_YP3_DIR/ffmpeg-arm64"
 
 function source_converters() {
     while IFS= read -r -d '' lib_file; do
         source "$lib_file"
     done < <(find "$CONVERTERS_DIR" -type f -name "*.sh" -print0 | sort -z)
+}
+
+function pull_yp3_binaries() {
+    local name
+
+    mkdir -p "$FFMPEG_YP3_DIR"
+    
+    for platform in amd64 arm64; do
+        for binary in ffmpeg ffprobe; do
+            name="$binary-$platform"
+            [[ -f "$FFMPEG_YP3_DIR/$name" ]] && continue
+
+            wget -P "$FFMPEG_YP3_DIR" "https://github.com/tytydraco/static-ffmpeg-yp3-patch/releases/latest/download/$binary-$platform"
+            chmod +x "$FFMPEG_YP3_DIR/$binary-$platform"
+        done
+    done
 }
 
 function main() {
@@ -21,8 +39,7 @@ function main() {
         exit 1
     fi
 
-    # Pull latest submodules.
-    git submodule update --init --recursive --remote
+    pull_yp3_binaries
 
     source "$CONFIG_FILE"
     source_converters
