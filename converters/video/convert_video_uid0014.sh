0d26ee3bb7f7759a2dd9a4e47a781a1c3353fb1e
Tyler Nijmeh <tylernij@gmail.com>
2026-07-16 12:03:47 -0700

    SL6806: Enable smarter weighted p decisions
    Doesn't cost much but can improve prediction accuracies and therefore
    reduce residuals.
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- converters/video/convert_video_uid0014.sh
+++ converters/video/convert_video_uid0014.sh
@@ -39,7 +39,7 @@ function convert_video_uid0014() {
         -n
         -f avi
         -c:v libx264
-        -x264-params "mbtree=0:rc-lookahead=0:aq-mode=2:ipratio=2:psy=0:me=tesa:subme=11:trellis=2"
+        -x264-params "mbtree=0:rc-lookahead=0:aq-mode=2:ipratio=2:psy=0:me=tesa:subme=11:trellis=2:weightp=2"
         -profile:v baseline
         -filter:v
         "
