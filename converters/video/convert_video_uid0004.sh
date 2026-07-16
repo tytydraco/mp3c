30fa8ef34bee03afe1a1b6e6ea7cb820531fc985
Tyler Nijmeh <tylernij@gmail.com>
2026-07-16 11:46:00 -0700

    SL6806: Enable trellis for smaller files
    Costs encoding time but produces smaller files.
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- converters/video/convert_video_uid0004.sh
+++ converters/video/convert_video_uid0004.sh
@@ -39,7 +39,7 @@ function convert_video_uid0004() {
         -n
         -f avi
         -c:v libx264
-        -x264-params "mbtree=0:rc-lookahead=0:aq-mode=2:ipratio=2:psy=0:me=tesa:subme=11"
+        -x264-params "mbtree=0:rc-lookahead=0:aq-mode=2:ipratio=2:psy=0:me=tesa:subme=11:trellis=2"
         -profile:v baseline
         -filter:v
         "
