d9bc39717384f4f54033b327e29154c27ebe7d99
Tyler Nijmeh <tylernij@gmail.com>
2026-07-17 09:59:30 -0700

    Better arch ffmpeg detection
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- main.sh
+++ main.sh
@@ -6,12 +6,7 @@ SCRIPT_DIR="$(dirname -- "$(readlink -f -- "${BASH_SOURCE[0]}")")"
 WORKING_DIR="$SCRIPT_DIR/working"
 CONVERTERS_DIR="$SCRIPT_DIR/converters"
 CONFIG_FILE="$SCRIPT_DIR/config.sh"
-
 FFMPEG_YP3_DIR="$SCRIPT_DIR/tools/ffmpeg/yp3_patch"
-FFMPEG_YP3="$FFMPEG_YP3_DIR/ffmpeg-amd64" # Or: WINEDEBUG=-all wine "$SCRIPT_DIR/tools/ffmpeg/vendor/ffmpeg-mod-shenju.exe"
-[[ "$(uname -m)" == "aarch64" || "$(uname -m)" == "arm64" ]] && FFMPEG_YP3="$FFMPEG_YP3_DIR/ffmpeg-arm64"
-
-#FFMPEG_YP3="/home/tytydraco/Projects/ffmpeg-yp3-patch/local/bin/ffmpeg"
 
 function source_converters() {
     while IFS= read -r -d '' lib_file; do
@@ -23,16 +18,27 @@ function pull_yp3_binaries() {
     local name
 
     mkdir -p "$FFMPEG_YP3_DIR"
-    
-    for platform in amd64 arm64; do
-        for binary in ffmpeg ffprobe; do
-            name="$binary-$platform"
-            [[ -f "$FFMPEG_YP3_DIR/$name" ]] && continue
 
-            wget -P "$FFMPEG_YP3_DIR" "https://github.com/tytydraco/static-ffmpeg-yp3-patch/releases/latest/download/$binary-$platform"
-            chmod +x "$FFMPEG_YP3_DIR/$binary-$platform"
-        done
-    done
+    function pull_arch_binary() {
+        [[ -f "$FFMPEG_YP3_DIR/$1" ]] && return 0
+        wget -P "$FFMPEG_YP3_DIR" "https://github.com/tytydraco/static-ffmpeg-yp3-patch/releases/latest/download/$1"
+        chmod +x "$FFMPEG_YP3_DIR/$1"
+    }
+
+    case "$(uname -m)" in
+        x86_64|amd64)
+            pull_arch_binary "ffmpeg-amd64"
+            FFMPEG_YP3="$FFMPEG_YP3_DIR/ffmpeg-amd64"
+            ;;
+        aarch64|arm64)
+            pull_arch_binary "ffmpeg-arm64"
+            FFMPEG_YP3="$FFMPEG_YP3_DIR/ffmpeg-arm64"
+            ;;
+        *)
+            echo "Unsupported architecture."
+            exit 1
+            ;;
+    esac
 }
 
 function convert_all() {
