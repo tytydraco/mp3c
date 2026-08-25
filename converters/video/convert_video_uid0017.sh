5d529bd1426a10ebbebba7eb4f2f10423b111be8
Tyler Nijmeh <tylernij@gmail.com>
2026-08-25 10:48:40 -0500

    Adjust bitrate for best viewing
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- converters/video/convert_video_uid0017.sh
+++ converters/video/convert_video_uid0017.sh
@@ -39,7 +39,7 @@ function convert_video_uid0017() {
         "
         -sws_flags "accurate_rnd+full_chroma_int+full_chroma_inp"
         -pix_fmt:v yuv420p
-        -b:v 150k
+        -b:v 200k
         -maxrate:v 1M
         -bufsize:v 1M
         -r:v "$fps"
